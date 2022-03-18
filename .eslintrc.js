63b0ccf6ca5965d51610cfb2c63014ceed7fb9a2
zeng.jian <isherry1993@hotmail.com>
2022-03-18 12:51:04 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
 			'tab'
 		],
 		'linebreak-style': [
-			'error',
+			'off',
 			'windows'
 		],
 		'quotes': [
@@ -38,6 +38,6 @@ module.exports = {
 			'error',
 			'always'
 		],
-		'no-mixed-spaces-and-tabs': 0
+		'no-mixed-spaces-and-tabs': 0,
 	}
 };
